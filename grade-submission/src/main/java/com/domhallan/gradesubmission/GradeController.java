05ffaef2b16c5dc6d220f8115f06f874daef0cb9
Dom Hallan <dom@domhallan.com>
2024-05-09 19:58:57 -0400

    Added new endpoint to fetch grades
    A new GET mapping has been added to the GradeController. This endpoint, "/grades", retrieves a grade object and adds it as an attribute to the model. The method then returns a string "grades".

--- grade-submission/src/main/java/com/domhallan/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/domhallan/gradesubmission/GradeController.java
@@ -1,7 +1,16 @@
 package com.domhallan.gradesubmission;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
+
+  @GetMapping("/grades")
+  public String getGrades(Model model) {
+    Grade grade = new Grade("Harry", "Potions", "A");
+    model.addAttribute("grade", grade);
+    return "grades";
+  }
 }
