38bb71f546728cc687d19c5d90dfac34fe2951f2
Dom Hallan <dom@domhallan.com>
2024-05-09 19:44:18 -0400

    Added Grade class with constructor
    A new class named 'Grade' has been added. This class includes three private fields: name, subject, and grade. A constructor for this class has also been implemented to initialize these fields.

---
+++ grade-submission/src/main/java/com/domhallan/gradesubmission/Grade.java
@@ -0,0 +1,13 @@
+package com.domhallan.gradesubmission;
+
+public class Grade {
+  private String name;
+  private String subject;
+  private String grade;
+
+  public Grade(String name, String subject, String grade) {
+    this.name = name;
+    this.subject = subject;
+    this.grade = grade;
+  }
+}
