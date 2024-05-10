7c5863da576b7299c926be901b27ab05354f0c24
Dom Hallan <dom@domhallan.com>
2024-05-09 20:33:23 -0400

    Refactor grade initialization in GradeController
    The student grades list is now initialized with values directly, instead of adding them later. This change simplifies the code and makes it easier to understand. The ArrayList has been replaced with Arrays.asList for this purpose.

--- grade-submission/src/main/java/com/domhallan/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/domhallan/gradesubmission/GradeController.java
@@ -4,19 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class GradeController {
 
-  List<Grade> studentGrades = new ArrayList<>();
+  List<Grade> studentGrades = Arrays.asList(
+    new Grade("Harry", "Potions", "A"),
+    new Grade("Hermione", "Transfiguration", "A"),
+    new Grade("Ron", "Divination", "D")
+  );
 
   @GetMapping("/grades")
   public String getGrades(Model model) {
-    studentGrades.add(new Grade("Harry", "Potions", "A"));
-    studentGrades.add(new Grade("Hermione", "Transfiguration", "A"));
-    studentGrades.add(new Grade("Ron", "Divination", "D"));
     Grade grade = new Grade("Harry", "Potions", "A");
     model.addAttribute("grades", studentGrades);
     return "grades";
