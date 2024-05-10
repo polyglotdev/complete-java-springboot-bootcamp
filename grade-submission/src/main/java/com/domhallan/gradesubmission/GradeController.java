40e77b4fa97a128434f1404ba5005d7a4435bd08
Dom Hallan <dom@domhallan.com>
2024-05-09 20:27:43 -0400

    Added sample student grades to GradeController
    The GradeController now includes a list of sample student grades. The "/grades" endpoint has been updated to add these grades to the model before returning the "grades" view.

--- grade-submission/src/main/java/com/domhallan/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/domhallan/gradesubmission/GradeController.java
@@ -4,13 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class GradeController {
 
+  List<Grade> studentGrades = new ArrayList<>();
+
   @GetMapping("/grades")
   public String getGrades(Model model) {
+    studentGrades.add(new Grade("Harry", "Potions", "A"));
+    studentGrades.add(new Grade("Hermione", "Transfiguration", "A"));
+    studentGrades.add(new Grade("Ron", "Divination", "D"));
     Grade grade = new Grade("Harry", "Potions", "A");
-    model.addAttribute("grade", grade);
+    model.addAttribute("grades", studentGrades);
     return "grades";
   }
 }
