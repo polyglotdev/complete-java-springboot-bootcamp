557ddddf9ec2b1d0745bc1f6ff489660d6edf3d1
Dom Hallan <dom@domhallan.com>
2024-05-13 14:33:03 -0400

    Updated gradeForm method in GradeController
    The gradeForm method in the GradeController class has been updated to take additional parameters. These include 'name', 'subject', and 'grade'. The model attribute "grade" is now initialized with these new parameters.

--- grade-submission/src/main/java/com/domhallan/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/domhallan/gradesubmission/GradeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,8 +21,10 @@ public class GradeController {
   }
 
   @GetMapping("/")
-  public String gradeForm(Model model) {
-    model.addAttribute("grade", new Grade());
+  public String gradeForm(Model model, @RequestParam String name,
+                          @RequestParam String subject,
+                          @RequestParam String grade) {
+    model.addAttribute("grade", new Grade(name, subject, grade ));
     return "form";
   }
 
